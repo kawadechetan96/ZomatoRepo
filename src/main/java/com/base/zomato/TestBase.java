04f143c69a232b7312e2fe7397d5e13cd69d2af2
chetan kavade <vaibhavkavade98@gmail.com>
2021-12-04 14:41:25 +0530

    New changes added

--- src/main/java/com/base/zomato/TestBase.java
+++ src/main/java/com/base/zomato/TestBase.java
@@ -1,9 +1,25 @@
 package com.base.zomato;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 
 public class TestBase {
 	
 	public static WebDriver driver;
-
+	public static Properties pro;
+				
+								TestBase()
+								{
+									try{
+									File source =new File("E:/Selenium Programs/Zomatoapp/src/main/java/com/config/zomato/Configuration.property");
+									FileInputStream fis=new FileInputStream(source);
+									pro=new Properties();
+									pro.load(fis);
+									}catch(Exception e){System.out.println("Exception Catch:-"+e.getMessage());}
+								}
+	
+	
 }
